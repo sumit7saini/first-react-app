c0c43cf62fc28d6cceefde47f465d54c2223ce9b
sumitsainiatechnos <sumitsainiatechnos@gmail.com>
2021-06-06 04:45:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import About from './components/About'
+import Services from './components/Services'
 
-function App() {
-  return (
+// function App() {
+//   return (
+//     <div className="App">
+//       <Navbar/>
+//       <Hero/>
+//       <About/>
+//     </div>
+//   );
+// }
+
+const App = () => {
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Hero/>
+      <About/>
+      <Services/>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Hero = () => {
+	return(<div class="jumbotron text-center">
+  <h1>Company</h1> 
+  <p>We specialize in blablabla</p> 
+  <form class="form-inline">
+    <div class="input-group">
+      <input type="email" class="form-control" size="50" placeholder="Email Address" required />
+      <div class="input-group-btn">
+        <button type="button" class="btn btn-danger">Subscribe</button>
+      </div>
+    </div>
+  </form>
+</div>);
+}
+
+export default Hero;
